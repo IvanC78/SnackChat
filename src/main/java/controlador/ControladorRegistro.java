4b2be1379692b6804bfd0e09572559617daaa891
David Re <gontzaldiaz@gmail.com>
2026-01-27 13:45:38 +0100

    Panel registro terminado. Se añaden los usuarios a la base de datos y hay mensajes de error si el telefono esta ya registrado. -Gontzal

--- src/main/java/controlador/ControladorRegistro.java
+++ src/main/java/controlador/ControladorRegistro.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import modelo.ServicioRegistro;
@@ -27,6 +31,11 @@ public class ControladorRegistro {
 	private TextField telefono;
 	@FXML
 	private Button registrarse;
+	@FXML
+	private Pane panel;
+	private Parent root;
+	private Stage stage;
+	private Scene scene;
 	
 	
 	Alert alerta = new Alert(Alert.AlertType.WARNING);
@@ -59,11 +68,18 @@ public class ControladorRegistro {
 		telefonoEsNumerico(telefonoS);
 		telefonoEsValido(telefonoS);
 		
+		if (!ServicioRegistro.telefonoDisponible(telefonoS)) { 
+			alertaError.setHeaderText("Teléfono duplicado"); 
+			alertaError.setContentText("¡Este teléfono ya esta registrado!"); 
+			alertaError.showAndWait(); 
+			return; 
+		}
+		
 		ServicioRegistro.registrar(usuarioS, contrasenyaS, telefonoS, colorC.toString());
 		alertaCorrecta.setHeaderText("Registro completado"); 
 		alertaCorrecta.setContentText("Usuario registrado correctamente"); 
 		alertaCorrecta.showAndWait();
-		// HAY QUE HACER QUE LA PESTAÑA SE CIERRE CUANDO LE DAS A ACEPTAR
+		volver();
 		
 		
 	}
@@ -93,6 +109,13 @@ public class ControladorRegistro {
 	    }
 
 	    return true;
+	}
+	
+	public void volver() throws IOException {
+	    
+	    Stage stage = (Stage) registrarse.getScene().getWindow(); 
+	    stage.close();
+	    
 	}
 
 	
